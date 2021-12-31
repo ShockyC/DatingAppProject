4aab9c2c12e4536a0a454f16bb9743f62627250c
JED WAN 2001548C <2001548C@student.tp.edu.sg>
2021-12-31 20:49:18 +0800

    Created Base Application - Login/Register/LandingPage. Done up to Lab5-Practice 82.

---
+++ Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DatingAppProject.Server.Data;
+using DatingAppProject.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DatingAppProject.Server.Areas.Identity.IdentityHostingStartup))]
+namespace DatingAppProject.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- Server/Data/Migrations/20211231124225_AddedNameToUser.Designer.cs
+++ Server/Data/Migrations/20211231124225_AddedNameToUser.Designer.cs
@@ -4,14 +4,16 @@ using DatingAppProject.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingAppProject.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211231124225_AddedNameToUser")]
+    partial class AddedNameToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
