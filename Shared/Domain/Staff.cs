45ba098e651b8350eaa560fd34a2e47708409060
JED WAN 2001548C <2001548C@student.tp.edu.sg>
2022-01-17 15:38:05 +0800

    Added Staff.cs, Client.cs, Comaplaint.cs to Domain

---
+++ Shared/Domain/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingAppProject.Shared.Domain
+{
+    public class Staff : BaseDomainModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string EducationLevel { get; set; }
+        public string Position { get; set; }
+        public int Contact { get; set; }
+    }
+}
