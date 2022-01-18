fe0da91784519b763b433c430b23637e3f273fcb
LIM RUI HOW, FABIAN 1900418C <1900418C@student.tp.edu.sg>
2022-01-18 09:29:50 +0800

    fixed typo of createdby in domain

--- Shared/Domain/BaseDomainModel.cs
+++ Shared/Domain/BaseDomainModel.cs
@@ -11,7 +11,7 @@ namespace DatingAppProject.Shared.Domain
         public int Id { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateUpdated { get; set; }
-        public string CreadedBy { get; set; }
+        public string CreatedBy { get; set; }
         public string UpdatedBy { get; set; }
 
     }
