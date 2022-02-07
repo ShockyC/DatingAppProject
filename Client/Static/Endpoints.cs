b07a178e8f761f4153c992c0f57304db90bdba72
JED WAN 2001548C <2001548C@student.tp.edu.sg>
2022-02-07 15:07:57 +0800

    Added endpoints just incase

---
+++ Client/Static/Endpoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAppProject.Client.Static
+{
+    public class Endpoints
+    {
+        public static readonly string Prefix = "api";
+
+        public static readonly string CustomersEndpoint = $"{Prefix}/customers";
+        public static readonly string StaffsEndpoint = $"{Prefix}/staffs";
+        public static readonly string ComplaintsEndpoint = $"{Prefix}/complaints";
+    }
+}
